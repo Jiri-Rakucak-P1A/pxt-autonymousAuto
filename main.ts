be163c46a84cf7797203dd604f169a16a8f78755
Jiri-Rakucak-P1A <115144255+Jiri-Rakucak-P1A@users.noreply.github.com>
2023-05-11 08:53:23 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,42 +3,77 @@
 
     let leftldx = 0
     let rightldx = 0
-    const speed = [0, -210]
-    const speed2 = [0, -225]
+    const speed = [0,-255]
+    const speed2 = [0,-240]
+    const nspeed = [240]
+    const nspeed2 = [255]
+    const stop = [0]
+    let block = false
+    let block1 = false
 
-    function car_motor(lm: number = 0, rm: number = 0) {
-        const ul = Math.map (lm, -100, 100, -255, 255)
-        const ur = Math.map(rm, -100, 100, -255, 255)
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, -ul)
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, -ur)
-    }
 
-radio.onReceivedValue(function(name: string, value: number) {
-    if (name = "")
-    PCAmotor.MotorRun(m1, speed[++rightldx % speed.length])
-    PCAmotor.MotorRun(m4, speed2[++leftldx % speed.length])
+
+    
+
+
+
+
+input.onButtonPressed(Button.A, function () {
+    control.inBackground(() => {
+        PCAmotor.MotorRun(m1, speed[++rightldx % speed.length])
+        PCAmotor.MotorRun(m4, speed2[++leftldx % speed.length])
+    })
+
+     basic.pause(1000)
+
+    control.inBackground(() => {
+        PCAmotor.MotorRun(m1, nspeed[++rightldx % speed.length])
+        PCAmotor.MotorRun(m4, nspeed2[++leftldx % speed.length])
+    })
+    
+
+    
+
+    
+
+    
+    
 })
+
+control.inBackground(() => {
+    
+})
+
 
 input.onButtonPressed(Button.A, function() {
-
-    PCAmotor.MotorRun(m1, speed[++rightldx % speed.length])
-    PCAmotor.MotorRun(m4, speed2[++leftldx % speed.length])
-   
-    PCAmotor.MotorRun(m4, speed[++rightldx % speed.length])
-    PCAmotor.MotorRun(m1, speed2[++leftldx % speed.length])
     
-    PCAmotor.MotorRun(m1, speed[++rightldx % speed.length])
-    PCAmotor.MotorRun(m4, speed2[++leftldx % speed.length])
-
 })
 
-        
+
+
+    
 
 
 
    
-        
+ /*       
+if (x < 4) { x++ }
+if (x == 4) { x = 1 }
 
 
+if (x == 2) {
+    PCAmotor.MotorRun(m1, speed[++rightldx % speed.length])
+    PCAmotor.MotorRun(m4, speed2[++leftldx % speed.length])
+}
+
+if (x == 3) {
+    PCAmotor.MotorRun(m1, nspeed[++rightldx % speed.length])
+    PCAmotor.MotorRun(m4, nspeed2[++leftldx % speed.length])
+}
+if (x == 1) {
+    PCAmotor.MotorRun(m1, stop[++rightldx % speed.length])
+    PCAmotor.MotorRun(m4, stop[++leftldx % speed.length])
+}
+*/
 
 
