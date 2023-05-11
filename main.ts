ef91ee8b46086eeb9caf0a81306493d6ece6aea0
Jiri-Rakucak-P1A <115144255+Jiri-Rakucak-P1A@users.noreply.github.com>
2023-05-11 11:38:03 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,93 @@
+    radio.setGroup(160)
+    
     let m1 = PCAmotor.Motors.M1
     let m4 = PCAmotor.Motors.M4
 
     let leftldx = 0
     let rightldx = 0
-    const speed = [0,-255]
-    const speed2 = [0,-240]
-    const nspeed = [240]
-    const nspeed2 = [255]
-    const stop = [0]
-    let block = false
-    let block1 = false
+    let speed = [0, -255]
+    let speed2 = [0, -240]
+    let nspeed = [0, 240]
+    let nspeed2 = [0, 255]
+    
+
+radio.onReceivedString(function(receivedString: string) {
+    if (receivedString == "go") {
+        PCAmotor.MotorRun(m1, speed[++rightldx % speed.length])
+        PCAmotor.MotorRun(m4, speed2[++leftldx % speed.length])
+    }
+    if (receivedString == "back"){
+        PCAmotor.MotorRun(m1, nspeed[++rightldx % speed.length])
+        PCAmotor.MotorRun(m4, nspeed2[++leftldx % speed.length])
+    }
+    
+
+})
+
+
+
+
+// radio.onReceivedValue(function(name: string, value: number) {
+//     if (name = "speed"){
+//         speed.unshift(value)
+//         speed2.unshift(value - (value * 0.059))
+//         speed.pop()
+//         speed2.pop()
+//     }
+//     if (name = "brake") {
+//         speed.push(value)
+//         speed2.push(value)
+//         speed.shift()
+//         speed2.shift()
+        
+//     }
+
+//     if (name = "turn") {
+//         if (value < 0 || value > -1023) {
+//             let neco = speed[value]
+//             speed.push(speed[value] + 100)
+            
+//         }
+
+//         if (value > 0 || value < 1023) {
+//             speed2.push(speed[value] + 100)
+//         }
+//     }
+
+    
+// })
+// basic.forever(function() {
+//     PCAmotor.MotorRun(m1, speed[++rightldx % speed.length])
+//     PCAmotor.MotorRun(m4, speed2[++leftldx % speed.length])
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
     
 
 
-
+/*
 
 input.onButtonPressed(Button.A, function () {
     control.inBackground(() => {
@@ -31,12 +102,6 @@ input.onButtonPressed(Button.A, function () {
         PCAmotor.MotorRun(m4, nspeed2[++leftldx % speed.length])
     })
     
-
-    
-
-    
-
-    
     
 })
 
@@ -49,7 +114,7 @@ input.onButtonPressed(Button.A, function() {
     
 })
 
-
+*/
 
     
 
