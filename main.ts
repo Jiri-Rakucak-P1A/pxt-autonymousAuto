03a3b35cfd4415ccc6141aaeb54f27c25c673c87
Jiri-Rakucak-P1A <115144255+Jiri-Rakucak-P1A@users.noreply.github.com>
2023-05-21 18:12:21 +0200

    fungujeto

--- main.ts
+++ main.ts
@@ -1,95 +1,35 @@
-let m1 = PCAmotor.Motors.M1
-let m4 = PCAmotor.Motors.M4
 radio.setGroup(160)
-let leftldx = 0
-let rightldx = 0
-let SpeedLeft = 0
-let SpeedRight = 0
+let speed1 = 0
+let speed2 = 0
+let nasobitel = 0
 
+radio.onReceivedValue(function (name: string, value: number) {
+    nasobitel = value
+    console.log(nasobitel)
+})
+basic.forever(function () {
 
-radio.onReceivedValue(function(name: string, value: number) {
-    if (name == "GOL"){
-        SpeedLeft = value
+    radio.onReceivedString(function (receivedString: string) {
+        if (receivedString === "Go") {
+            speed1 = -255 * nasobitel
+            speed2 = (speed1 * 0.92) * nasobitel
         }
-    if (name == "GOR") {
-        SpeedRight = value
-    }
-    if (name == "ReverseL") {
-        SpeedLeft = value
-    }
-    if (name == "ReverseR") {
-        SpeedRight = value
-    }
-    if (name == "Left") {
-        SpeedLeft = value
-    }
-    if (name == "Right") {
-        SpeedRight = value
-    }
-    radio.onReceivedString(function(receivedString: string) {
-        if(receivedString === "Stop"){
-            PCAmotor.MotorStopAll()
+
+        if (receivedString === "L") {
+            speed1 = -100 * nasobitel
         }
+        if (receivedString === "R") {
+            speed2 = -80 * nasobitel
+        }
+        if (receivedString === "LGo") {
+            speed1 = -255 * nasobitel
+        }
+        if (receivedString === "RGo") {
+            speed2 = (-255 * 0.92) * nasobitel
+        }
+
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, speed1)
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, speed2)
+
     })
 })
-basic.forever(function() {
-    PCAmotor.MotorRun(PCAmotor.Motors.M1, SpeedLeft)
-    PCAmotor.MotorRun(PCAmotor.Motors.M4, SpeedRight)
-})
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// radio.onReceivedValue(function(name: string, value: number) {
-//     if (name = "speed"){
-//         speed.unshift(value)
-//         speed2.unshift(value - (value * 0.059))
-//         speed.pop()
-//         speed2.pop()
-//     }
-//     if (name = "brake") {
-//         speed.push(value)
-//         speed2.push(value)
-//         speed.shift()
-//         speed2.shift()
-        
-//     }
-
-//     if (name = "turn") {
-//         if (value < 0 || value > -1023) {
-//             let neco = speed[value]
-//             speed.push(speed[value] + 100)
-            
-//         }
-
-//         if (value > 0 || value < 1023) {
-//             speed2.push(speed[value] + 100)
-//         }
-//     }
-
-    
-// })
-// basic.forever(function() {
-//     PCAmotor.MotorRun(m1, speed[++rightldx % speed.length])
-//     PCAmotor.MotorRun(m4, speed2[++leftldx % speed.length])
-// })
