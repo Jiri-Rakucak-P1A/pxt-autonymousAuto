72d44fbda928769eedbbb2e6c38cd789bae6f609
Jiri-Rakucak-P1A <115144255+Jiri-Rakucak-P1A@users.noreply.github.com>
2023-06-21 12:23:17 +0200

    hotovo

--- main.ts
+++ main.ts
@@ -1,86 +1,53 @@
-// radio.setGroup(160)
-// let speed1 = 0
-// let speed2 = 0
-// let nasobitel = 0
-// let a = 0
-// let b = 0
-
-// radio.onReceivedValue(function (name: string, value: number) {
-//     if (name === "n"){
-//     nasobitel = value  
-//     }
-// })
-// basic.forever(function () {
-
-//     radio.onReceivedString(function (receivedString: string) {
-//         if (receivedString === "Go") {
-//             speed1 = -255 * nasobitel
-//             speed2 = (speed1 * 0.92) * nasobitel
-//         }
-
-//         if (receivedString === "L") {
-//             speed1 = -100 * nasobitel
-//         }
-//         if (receivedString === "R") {
-//             speed2 = -50 * nasobitel
-//         }
-//         if (receivedString === "LGo") {
-//             speed1 = -255 * nasobitel
-//         }
-//         if (receivedString === "RGo") {
-//             speed2 = (-255 * 0.92) * nasobitel
-//         }
-
-//         PCAmotor.MotorRun(PCAmotor.Motors.M1, speed1)
-//         PCAmotor.MotorRun(PCAmotor.Motors.M4, speed2)
-
-//     })
-// })
-
-
-
-
-pins.setPull(DigitalPin.P15, PinPullMode.PullNone)
-pins.setPull(DigitalPin.P14, PinPullMode.PullNone)
-pins.setPull(DigitalPin.P13, PinPullMode.PullNone)
-
-const c: DigitalPin = DigitalPin.P15
-const l: DigitalPin = DigitalPin.P14
-const r: DigitalPin = DigitalPin.P13
-
-let c1: number = 1
-let r1: number = 1
-let l1: number = 1
-
-let speed = 0
+radio.setGroup(160)
+let speed1 = 0
 let speed2 = 0
+let index = 0
+let a = 0
+let b = 0
+let serialnum = 0 // číslo microbitu
+function motor_run(left: number = 0, right: number = 0) {
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, speed1)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, speed2)
+}
+basic.forever(function () {
+    radio.onReceivedString(function (receivedString: string) {
+        if (serialnum != radio.receivedPacket(RadioPacketProperty.SerialNumber)) return
+        if (receivedString === "plus"){
+            index + 0.5
+        }
+        if (receivedString === "minus"){
+            index - 0.5
+        }
+        if (receivedString === "Go") {
+            speed1 = -255 * index
+            speed2 = (speed1 * 0.92) * index
+            motor_run
+        }
 
 
-basic.forever(function() {
-    c1 = pins.digitalReadPin(c)
-    l1 = pins.digitalReadPin(l)
-    r1 = pins.digitalReadPin(r)
 
-    if (c1 === 0){
-        speed = -150
-        speed2 = -130
-        console.log(1)
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, speed)
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, speed2)
-    }
-    if (r1 === 0){
-        speed = -150
-        speed2 = -80
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, speed)
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, speed2)
-    }
-    if (l1 === 0){
-        speed = -100
-        speed2 = -130
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, speed)
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, speed2)
+        if (receivedString === "L") {
+            speed1 = -100 * index
+            motor_run
+        }
+        if (receivedString === "R") {
+            speed2 = -50 * index
+            motor_run
+        }
+        if (receivedString === "LGo") {
+            speed1 = -255 * index
+            motor_run
+        }
+        if (receivedString === "RGo") {
+            speed2 = (-255 * 0.92) * index
+            motor_run
+        }
+        if (receivedString === "Stop"){
+            speed1 = 0
+            speed2 = 0
+            index = 0
+            motor_run
+        }
 
-    }
-    basic.pause(50)
-
+    })
 })
